f81ec49407001e7a59344ec0861a5ca5e34620b3
SUHAIB-RASHID-DAR <132136593+SUHAIB-RASHID-DAR@users.noreply.github.com>
2023-05-27 15:09:38 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -18,7 +18,7 @@ from pyrogram.errors import FloodWait
 from ethon.pyfunc import video_metadata
 #from ethon.telefunc import force_sub
 
-ft = f"To use this bot you've to join @{fs}."
+#ft = f"To use this bot you've to join @{fs}."
 
 batch = []
 batch_ = []
